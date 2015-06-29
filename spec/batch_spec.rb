f44916d3dd41bec175ee92da97bf090656ffe6ee
Matt Brooke-Smith <matt@futureworkshops.com>
2015-06-29 21:59:42 +0100

    Update spec

--- spec/batch_spec.rb
+++ spec/batch_spec.rb
@@ -3,30 +3,27 @@ require 'spec_helper'
 describe Notifiable::Gcm::Spacialdb::Batch do
   
   let(:a) { Notifiable::App.create }  
-  let(:n1) { Notifiable::Notification.create(:message => "Test message", :app => a) }
-  let(:n1_with_params) { Notifiable::Notification.create(:message => "Test message", :app => a, :params => {:flag => true}) }
-  let(:d) { Notifiable::DeviceToken.create(:token => "ABC123", :provider => :gcm, :app => a) }
+  let(:n1) { Notifiable::Notification.create(:app => a) }
+  let!(:ln) { Notifiable::LocalizedNotification.create(:message => "Test message", :params => {:flag => true}, :notification => n1, :locale => :en) }
+  let(:d) { Notifiable::DeviceToken.create(:token => "ABC123", :provider => :gcm, :app => a, :locale => :en) }
   
   it "sends a single gcm notification" do    
     stub_request(:post, "https://android.googleapis.com/gcm/send").to_return(:body => '{ "multicast_id": 108, "success": 1, "failure": 0, "canonical_ids": 0, "results": [{ "message_id": "1:08" }]}')   
     
-    n1.batch do |n|
-      n.add_device_token(d)
-    end
-    
+    n1.batch {|n| n.add_device_token(d)}
+        
     Notifiable::NotificationStatus.count.should == 1
     Notifiable::NotificationStatus.first.status = 0
   end
   
   it "sends custom attributes" do        
-    stub_request(:post, "https://android.googleapis.com/gcm/send")
-      .with(:body => {:registration_ids => ["ABC123"], :data => {:message => n1.message, :flag => true, :notification_id => n1_with_params.id}})
-      .to_return(:body => '{ "multicast_id": 108, "success": 1, "failure": 0, "canonical_ids": 0, "results": [{ "message_id": "1:08" }]}')   
+      stub_request(:post, "https://android.googleapis.com/gcm/send")
+               .with(:body => "{\"registration_ids\":[\"ABC123\"],\"data\":{\"message\":\"Test message\",\"flag\":true,\"localized_notification_id\":1}}")
+               .to_return(:body => '{ "multicast_id": 108, "success": 1, "failure": 0, "canonical_ids": 0, "results": [{ "message_id": "1:08" }]}') 
     
-    n1_with_params.batch do |n|
-      n.add_device_token(d)
-    end
-      
+    n1.batch {|n| n.add_device_token(d)}
+   
+   
     Notifiable::NotificationStatus.count.should == 1
     Notifiable::NotificationStatus.first.status = 0
   end
@@ -34,9 +31,7 @@ describe Notifiable::Gcm::Spacialdb::Batch do
   it "marks a unregistered token as invalid" do    
     stub_request(:post, "https://android.googleapis.com/gcm/send").to_return(:body => '{ "multicast_id": 108, "success": 0, "failure": 1, "canonical_ids": 0, "results": [{ "error": "NotRegistered" }]}')  
         
-    n1.batch do |n|
-      n.add_device_token(d)
-    end
+    n1.batch {|n| n.add_device_token(d)}
         
     Notifiable::NotificationStatus.count.should == 1
     Notifiable::NotificationStatus.first.status = 4
@@ -46,9 +41,7 @@ describe Notifiable::Gcm::Spacialdb::Batch do
   it "marks an invalid token as invalid" do    
     stub_request(:post, "https://android.googleapis.com/gcm/send").to_return(:body => '{ "multicast_id": 108, "success": 0, "failure": 1, "canonical_ids": 0, "results": [{ "error": "InvalidRegistration" }]}')  
         
-    n1.batch do |n|
-      n.add_device_token(d)
-    end
+    n1.batch {|n| n.add_device_token(d)}
         
     Notifiable::NotificationStatus.count.should == 1
     Notifiable::NotificationStatus.first.status = 2
@@ -58,9 +51,7 @@ describe Notifiable::Gcm::Spacialdb::Batch do
   it "updates a token to the canonical ID if it does not exist" do   
     stub_request(:post, "https://android.googleapis.com/gcm/send").to_return(:body => '{ "multicast_id": 108, "success": 1, "failure": 0, "canonical_ids": 1, "results": [{ "message_id": "1:08", "registration_id": "GHJ12345" }]}')  
         
-    n1.batch do |n|
-      n.add_device_token(d)
-    end
+    n1.batch {|n| n.add_device_token(d)}
         
     Notifiable::NotificationStatus.count.should == 1
     Notifiable::NotificationStatus.first.status = 0
@@ -87,10 +78,8 @@ describe Notifiable::Gcm::Spacialdb::Batch do
   it "deals gracefully with an unauthenticated key" do    
     stub_request(:post, "https://android.googleapis.com/gcm/send").to_return(:body => '<html>Message</html>', :status => 401)  
         
-    n1.batch do |n|
-      n.add_device_token(d)
-    end
-        
+    n1.batch {|n| n.add_device_token(d)}
+
     Notifiable::NotificationStatus.count.should == 0
   end 
   
